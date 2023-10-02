3ca0bcc9c9515cebc7d88f8bc64132d28688cb67
WISDOM MMADUABUCHI <133394509+Echewis@users.noreply.github.com>
2023-10-02 20:24:30 +0100

    making use of slicing in python

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 first_3 = word[:3]
 last_2 = word[-2:]
-middle_word = word[1:8]
+middle_word = word[1:-1]
 print(f"First 3 letters: {first_3}")
 print(f"Last 2 letters: {last_2}")
 print(f"Middle word: {middle_word}")
