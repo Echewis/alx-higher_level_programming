338585eb66c4380a1f54a53795edf33c22506e51
WISDOM MMADUABUCHI <133394509+Echewis@users.noreply.github.com>
2023-10-02 21:39:43 +0100

    Update reasigning value to a variable in python using slicing

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
-str = str[39:46] + str[47:66] + str[106:112] + str[0:6]
+str = str[39:66] + str[106:112] + str[0:6]
 print(str)
