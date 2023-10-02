56384ad8f9de6896beca2583bbeb8743d70d4978
WisDomz <houseofwem1@gmail.com>
2023-10-02 20:04:24 +0100

    This is a hands on project on how to use slicing in python

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first = word[:3]
+mid = word[1:8]
+last = word[7:9]
+print(f"First 3 letters: {first}")
+print(f"Last 2 letters: {last}")
+print(f"Middle word: {mid}")
