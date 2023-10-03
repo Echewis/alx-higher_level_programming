66556649c1c780a21335052298fcf78d5e9a0413
WISDOM MMADUABUCHI <133394509+Echewis@users.noreply.github.com>
2023-10-03 22:08:12 +0100

    Update 12-fizzbuzz challenge

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for a in range(1, 101):
         if a % 3 == 0 and a % 5 == 0:
-            print("FizzBuzz", end=" ")
+            print("FizzBuzz ", end="")
         elif a % 3 == 0:
-            print("Fizz", end=" ")
+            print("Fizz ", end="")
         elif a % 5 == 0:
-            print("BUzz", end=" ")
+            print("BUzz ", end="")
         else:
-            print("{}".format(a), end=" ")
+            print("{} ".format(a), end="")
