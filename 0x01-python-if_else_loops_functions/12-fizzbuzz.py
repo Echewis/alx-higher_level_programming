c685a32d22f5c24b8b6d7ff2ae3fec7904ef4d91
WisDomz <houseofwem1@gmail.com>
2023-10-03 21:55:59 +0100

    Fizzbuzz challenge

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/us/bin/python3
+def fizzbuzz():
+    for a in range(1, 101):
+        if a % 3 == 0 and a % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif a % 3 == 0:
+            print("Fizz", end=" ")
+        elif a % 5 == 0:
+            print("BUzz", end=" ")
+        else:
+            print("{}".format(a), end=" ")
