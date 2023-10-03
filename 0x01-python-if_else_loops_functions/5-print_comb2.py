9626274d5f174eecfb915f48e55a2925a21bd94d
WisDomz <houseofwem1@gmail.com>
2023-10-03 16:59:26 +0100

    a program that prints numbers from 0 to 99 in 2 decimal format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for numz in range(100):
-    print("{:02}".format(numz), end="")
     if numz != 99:
-        print(", ", end="")
-print()
+        print("{:02}".format(numz), end=", ")
+    else:
+        print("{}".format(numz))
