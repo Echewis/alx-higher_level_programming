9b47fcd08205775a2d19f0ef1547fefcb4c70021
WISDOM MMADUABUCHI <133394509+Echewis@users.noreply.github.com>
2023-10-05 12:35:03 +0100

    Update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     len_arg = len(sys.argv)
     if len_arg == 1:
         print("{} arguments.".format(len_arg - 1))
-    else:
+    elif len_arg >= 2:
         print("{} arguments:".format(len_arg - 1))
     for a in range(1, len_arg):
         print("{}: {}".format(a, sys.argv[a]))
