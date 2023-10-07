898b70ecc1629337f36f853468a7b58e648d5062
WISDOM MMADUABUCHI <133394509+Echewis@users.noreply.github.com>
2023-10-07 13:27:16 +0100

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,8 +2,3 @@
 def print_list_integer(my_list=[]):
     for n in range(len(my_list)):
         print("{:d}".format(my_list[n]))
-    """ another way is
-    for n in my_list:
-        if isinstance(n, int):
-        print("{}".format(n))
-        """
