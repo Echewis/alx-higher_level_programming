b3d8348d8844793ed81d866646e908c311355fe0
WisDomz <houseofwem1@gmail.com>
2023-10-09 18:41:08 +0100

    CPython is the reference implementation of the Python programming language

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,22 @@
+#include <python.h>
+/**
+ * print_python_list_info - This willl print the pyrhon list
+ * @p: is the A PyObect list
+ */
+void print_python_list_info(PyObject *p)
+{
+	int how_big, allot, a;
+	PyObject *o;
+	
+	how_big = Py_SIZE(p);
+	allot = ((PyListObect *)p)->allotment;
+
+	printf(("[*] Size of the Python List = %d\n", how_big);
+	printf("[*] Allocated = %d\n", allot);
+
+	for (a = 0; a < how_big; a++)
+	{
+		o = PyList_Getltem(p, a);
+		printf("Element %d: %s\n", a, Py_TYPE(o)->tp_name);
+	}
+}
