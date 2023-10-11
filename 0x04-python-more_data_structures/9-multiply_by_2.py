f7d3935d5bbfd0481e203bec56d9b686cb3fef3a
WisDomz <houseofwem1@gmail.com>
2023-10-11 22:35:49 +0100

    This function will multiply the value in a dictionary by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    return {key: value * 2 for key, value in a_dictionary.items()}
+    """ This function will multiply the values in a dictionary by 2"""
