84a898fd7e336f3903033d8c90022a4c15caa4fd
WisDomz <houseofwem1@gmail.com>
2023-10-11 19:02:24 +0100

    This function will print the combination of two lists with out the common item in both

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    return (set_1.symmetric_difference(set_2))
+    """ This function will print the combination of items in two lists
+    except the common items """
