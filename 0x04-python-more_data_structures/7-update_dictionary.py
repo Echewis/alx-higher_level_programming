ed874b2f0e3b2a3f291e97826f5ff0c482882c2d
WisDomz <houseofwem1@gmail.com>
2023-10-11 22:08:31 +0100

    This function will add or replace the key and value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return (a_dictionary)
+    """ THis function will replace or add key and value to the dictionary """
