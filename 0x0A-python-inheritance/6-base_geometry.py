244d56e3665be955a558c7f1646bf7dbf97b5d3a
WisDomz <houseofwem1@gmail.com>
2023-11-06 19:23:34 +0100

    advanced BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" intialization of BaseGeometry """
+
+
+class BaseGeometry:
+    """ class defintion of BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implement")
