adb78f45ae404fdb24d384d0d3ceb77e5b5d03cc
WisDomz <houseofwem1@gmail.com>
2023-11-06 18:53:19 +0100

    inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" intialization of function inherits_from """
+
+
+def inherits_from(obj, a_class):
+    """ definition of function """
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
+
