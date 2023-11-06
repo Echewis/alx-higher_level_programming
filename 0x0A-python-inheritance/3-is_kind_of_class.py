6597a0e0917f2cc3a1efe710d22c7d76c1ab9298
WisDomz <houseofwem1@gmail.com>
2023-11-06 18:38:03 +0100

    object is an instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This fuction checks for instance of a class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Definition of a class """
+    return (isinstance(obj, a_class))
