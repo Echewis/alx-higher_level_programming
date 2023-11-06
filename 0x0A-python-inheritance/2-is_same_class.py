2191448e88a284fac8643874499df37fee611964
WisDomz <houseofwem1@gmail.com>
2023-11-06 17:43:06 +0100

    function that return a object instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This will check for isinstance of class """
+
+
+def is_same_class(obj, a_class):
+    """ This will check if class of isinstance of a class"""
+    return (type(obj) == a_class)
