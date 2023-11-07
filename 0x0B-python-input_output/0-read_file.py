5fa3cc28c9ca91e4ad317cafd82a81aa19c57d92
WisDomz <houseofwem1@gmail.com>
2023-11-07 15:38:43 +0100

    function that reads text

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This function reads a text file(UTF8) and prints to stdout """
+
+
+def read_file(filename=""):
+    """definition of function read_file """
+
+    with open(filename, "r", encoding="utf-8") as f:
+            print(f.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+# This directory includes projects on  Python input/output
