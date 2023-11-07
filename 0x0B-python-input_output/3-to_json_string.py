124ba00ba3e7386499b3be1e4d191063356f2980
WisDomz <houseofwem1@gmail.com>
2023-11-07 18:45:05 +0100

    JSON reprensation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""This function will return JSON representation of an object(string)"""
+
+
+def to_json_string(my_obj):
+    """definition of function to_json_string """
+    return (json.dumps(my_obj))
