962d27085b4603e8568e4c439d631f3f7a3e449b
WisDomz <houseofwem1@gmail.com>
2023-12-12 23:11:12 +0100

    Full creation of table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- a script that creates a table second_table in the database
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHARA(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 INSERT INTO second_table(id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table(id, name, score) VALUES (2, 'Alex', 3);
