fb4d6fcc4f807fc7602bfb0a2f08611994d6672e
WisDomz <houseofwem1@gmail.com>
2023-12-12 23:55:16 +0100

    Group item usage

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,4 @@
 -- a script that lists the number of records with the same score in the table
 SELECT score, COUNT('score') AS number FROM second_table
 GROUP BY score
-ORDER BY score
 ORDER BY score DESC;
