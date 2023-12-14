28e6c0e6f8141b20bbfde4a7952329f2bf7fa75e
WisDomz <houseofwem1@gmail.com>
2023-12-14 17:51:16 +0100

    genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- Import the database dump from hbtn_0d_tvshows
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can use only one SELECT statement
+SELECT tv_show.title, tv_show_genres.genre_id
+FROM tv_show
+INNER JOIN tv_show_genres
+ON tv_show.id = tv_show_genres.show_id
+ORDER BY tv.show.title, tv_show_genres.genre_id;
