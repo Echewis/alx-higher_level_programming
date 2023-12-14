c25ee146fd43ae23eb6bfa6da34daebd04ef32d7
WisDomz <houseofwem1@gmail.com>
2023-12-14 17:25:05 +0100

    states table contains only one record where name = California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- a script that lists all the cities of California that can be found in the database hbtn_0d_usa
 -- The states table contains only one record where name = California (but the id can be different, as per the example)
-SELECT * FROM cities WHERE state_id IN
+SELECT id, name FROM cities WHERE state_id IN
 (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
