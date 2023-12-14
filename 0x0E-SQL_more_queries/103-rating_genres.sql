e1594e77cc91073d2a616f802dc9970bf01713b8
WisDomz <houseofwem1@gmail.com>
2023-12-14 18:04:52 +0100

    Import the database dump

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,9 @@
+-- mport the database dump from hbtn_0d_tvshows_rate to your MySQL server
+SELECT name, SUM(rate) AS Rate
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_show_ratings
+ON tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY name
+ORDER BY rating DESC;
