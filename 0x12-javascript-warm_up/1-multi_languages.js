b99935f5bc42e86dc92f391fbc777e5a55088a20
WISDOM MMADUABUCHI <133394509+Echewis@users.noreply.github.com>
2024-01-08 17:13:11 +0100

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-// A script that will print three different sentences
+// A script that will print three different sentences.
 
 const myVar = "C is fun\nPython is cool\nJavaScript is amazing";
 console.log(myVar);
