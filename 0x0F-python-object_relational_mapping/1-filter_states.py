b74074bcc4fa9e47ee38bcfa0d7a84977922f3b9
WisDomz <houseofwem1@gmail.com>
2024-01-15 14:05:15 +0100

    1-filter_states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+It will list all the states from the database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    This will give access to the data base and get the state from db
+    """
+    datadata = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                               passwd=argv[2], datadata=argv[3])
+
+    curd = datadata.cursor()
+    curd.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY 'N%' \
+                 ORDER BY states.id ASC")
+    rows = curd.fetchall()
+
+    for ro in rows:
+        print(ro)
