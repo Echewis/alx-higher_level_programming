27608f801f53fb4ca0591a3593f0eb4cb11e282d
WisDomz <houseofwem1@gmail.com>
2024-01-25 14:41:41 +0100

    advance

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed
-curl -s -H "Content-Type: application/json" -d "$(cat "$2") "$1"
+curl -X POST -H "Content-Type: application/json" -d @$2 $1
