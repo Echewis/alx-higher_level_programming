2925b3f602eb88c56fb9c7c19d6af5fbaee2e4b0
WisDomz <houseofwem1@gmail.com>
2024-01-27 00:04:52 +0100

    response header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This will displays the X-Request-Id header variable of a request to a given URL.
+"""This will displays the X-Request-Id header variable
+of a request to a given URL.
 """
 import sys
 import requests
