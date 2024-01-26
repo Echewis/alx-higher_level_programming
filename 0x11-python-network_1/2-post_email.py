d3b3975519a40f73fef7228dcfb1dff815531578
WisDomz <houseofwem1@gmail.com>
2024-01-27 00:24:31 +0100

    posting

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""This will send a post request to a certai url
+"""This code will sends a POST request to a given URL with a given email
 """
-import urllib.request
-import urllib.parse
 import sys
+import urllib.parse
+import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
     val = {"email": sys.argv[2]}
-    info = urllib.parse.urlencode(val).encode("ascii")
+    da = urllib.parse.urlencode(val).encode("ascii")
 
-    request = urllib.request.Request(url, info)
+    request = urllib.request.Request(url, da)
     with urllib.request.urlopen(request) as response:
         print(response.read().decode("utf-8"))
