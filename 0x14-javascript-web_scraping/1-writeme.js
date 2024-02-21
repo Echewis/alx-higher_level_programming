cab82a52400c0684b75bbcad5cfac30b58dcba4c
WisDomz <houseofwem1@gmail.com>
2024-02-21 16:32:13 +0100

    task two

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
+const s = require('s');
+s.writeFile(process.argv[2], process.argv[3], error => {
   if (error) console.log(error);
 });
